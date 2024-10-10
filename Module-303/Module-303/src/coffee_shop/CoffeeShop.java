b44d276c63f6edc20a0a59726123651a61eb7d4e
Anjaom <eric@webworkbench.net>
2024-10-10 12:50:02 -0600

    added a basic user input main menu

--- Module-303/Module-303/src/coffee_shop/CoffeeShop.java
+++ Module-303/Module-303/src/coffee_shop/CoffeeShop.java
@@ -2,8 +2,12 @@ package coffee_shop;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class CoffeeShop {
+
+    // we will use this is a couple functions to read input from the user
+    private Scanner scanner = new Scanner(System.in);
 
     // this is a class level variable that will contain our list of products for sale
     // best practice is to define these at the top of the class
@@ -30,17 +34,50 @@ public class CoffeeShop {
             // before using it to create the string that is printed
             System.out.println((count+1) + ") " + p.getName() + " \t " + p.getPrice());
         }
+
+        // give some white space after print
+        System.out.println("");
     }
 
+    private int printMainMenu() {
+        System.out.println("1) See product menu");
+        System.out.println("2) Purchase product");
+        System.out.println("3) Checkout");
+        System.out.println("4) Exit");
 
+        System.out.print("\nEnter Selection :");
+
+        int selection = scanner.nextInt();
+
+        return selection;
+    }
 
     public void start() {
         // this becomes similar to the main method in that it will be where our project starts and runs
         // 1) initialize the products for sale
         initProducts();
 
-        // 2) print the list of products for sale
-        printProductMenu();
+        // repeat forever until the user enters selection 4 which will exit the program
+        while( true ) {
+            // print the menu and get back the user selected input
+            int selection = printMainMenu();
+
+            if (selection == 1) {
+                // print the product menu
+                printProductMenu();
+            } else if (selection == 2) {
+                // purchase product / add to cart
+            } else if (selection == 3) {
+                // checkout
+            } else if (selection == 4) {
+                System.out.println("Good bye");
+
+                // we are exiting with a value of 0 means successful exit
+                // this ends the program
+                System.exit(0);
+            }
+        }
+
     }
 
     public static void main(String[] args) {
