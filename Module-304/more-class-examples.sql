0be1347f1e1b9b2325b2629c1f4db970c7e527e7
Anjaom <eric@webworkbench.net>
2024-10-31 14:46:08 -0600

    more query examples

--- Module-304/more-class-examples.sql
+++ Module-304/more-class-examples.sql
@@ -115,3 +115,147 @@ select * from customers where customer_name like 'A%';
 select phone from customers where customer_name like 'A%'
 union
 select firstname from employees;
+
+--  ==================================================
+
+select c.customer_name AS Customer, 
+	SUM(od.quantity_ordered * od.price_each) AS Due, 
+	COALESCE(SUM(p.amount), 0) AS Paid, -- could be accomplished with a subquery
+	SUM(od.quantity_ordered * od.price_each) - COALESCE(SUM(p.amount), 0) AS Balance
+FROM customers c
+JOIN orders o on c.id = o.customer_id
+JOIN orderdetails od ON o.id = od.order_id
+LEFT JOIN payments p ON c.id = p.customer_id
+GROUP BY c.id, c.customer_name
+HAVING Balance > 0
+ORDER BY Balance DESC;
+
+-- any customer that has not made a payment ever will not appear in this query .. this is a high priority because it has a high probablility of happening.
+-- any customer that has made a payment but never made an order would also never appear in this query ... low level problem because its not likely to exist
+select c.id, c.contact_firstname, c.contact_lastname, 
+	sum(quantity_ordered * price_each) as total_purchase, 
+	( select sum(pm.amount) from payments pm where pm.customer_id = c.id) as total_payment, 
+if(sum(quantity_ordered * price_each) > ( select sum(pm.amount) from payments pm where pm.customer_id = c.id), 
+(sum(quantity_ordered * price_each) - ( select sum(pm.amount) from payments pm where pm.customer_id = c.id)), 'No outstanding' ) as outstanding_balance 
+from customers c, orderdetails od, orders o
+where  o.customer_id = c.id
+    and od.order_id = o.id
+group by c.id
+order by c.id;
+
+
+select c.id, c.contact_firstname, c.contact_lastname, 
+sum(quantity_ordered * price_each) as total_purchase, total_payment, 
+if(sum(quantity_ordered * price_each) > total_payment, (sum(quantity_ordered * price_each) - total_payment), 'No outstanding' ) as outstanding_balance 
+from customers c, orderdetails od, orders o, 
+-- this is done less but still is possible
+	( select c.id, ( select sum(pm.amount) from payments pm where pm.customer_id = c.id) as total_payment
+	from customers c ) pm
+where pm.id = c.id
+	and o.customer_id = c.id
+    and od.order_id = o.id
+group by c.id
+order by c.id;
+
+-- to use a query as a subtable
+ select c.id, ( select sum(pm.amount) from payments pm where pm.customer_id = c.id) as total_payment
+	from customers c;
+
+
+select c.id, c.customer_name, sum(od.quantity_ordered * od.price_each) as amount_purchased, ( select sum(pm.amount) from payments pm where pm.customer_id = c.id) as amount_paid,
+ sum(od.quantity_ordered * od.price_each) - ( select sum(pm.amount) from payments pm where pm.customer_id = c.id) as balance
+from customers c, orders o, orderdetails od
+where
+o.customer_id = c.id
+and od.order_id = o.id
+group by o.customer_id;
+
+select * from payments where customer_id = 131;
+select * from orders where customer_id = 131;
+select * from orderdetails where order_id in ( 10107, 10248, 10292, 10329);
+
+--- 
+
+select c.id as customer_id,c.customer_name ,
+sum(od.quantity_ordered * od.price_each) as total_price,
+(sum(od.quantity_ordered*od.price_each)-sum(pay.amount)) as amount_due
+from customers c, payments pay, orders o, orderdetails od, products p
+where c.id = pay.customer_id
+and c.id = o.customer_id
+and o.id = od.order_id
+and od.product_id = p.id
+group by c.id
+order by amount_due desc;
+
+-- 
+
+SELECT c.id, 
+    c.customer_name AS Customer, 
+    od.total_due AS Due, 
+    COALESCE(p.total_paid, 0) AS Paid, 
+    od.total_due - COALESCE(p.total_paid, 0) AS Balance
+FROM customers c
+JOIN (
+    -- subquery he create customer id, total amount spent 
+    SELECT o.customer_id, 
+           SUM(od.quantity_ordered * od.price_each) AS total_due
+    FROM orders o
+    JOIN orderdetails od ON o.id = od.order_id
+    GROUP BY o.customer_id
+) od ON c.id = od.customer_id
+LEFT JOIN (
+    SELECT customer_id, 
+           SUM(amount) AS total_paid
+    FROM payments
+    GROUP BY customer_id
+) p ON c.id = p.customer_id
+ORDER BY Balance DESC;
+
+
+--   
+
+
+	SELECT EXTRACT(YEAR FROM o.order_date) AS Year, p.product_name, SUM(od.quantity_ordered * (od.price_each - p.buy_price)) AS Profit
+	FROM orderdetails od
+	JOIN orders o ON od.order_id = o.id
+	JOIN products p ON od.product_id = p.id
+	GROUP BY
+		Year, p.product_name
+	ORDER BY 
+		Year DESC, Profit DESC;
+       
+       
+select year(o.order_date), p.product_name, sum(od.quantity_ordered*(od.price_each - p.buy_price)) as profit
+from orderdetails od, products p, orders o
+where od.product_id = p.id
+and od.order_id = o.id
+group by year(o.order_date), od.product_id
+order by year(o.order_date) desc, profit desc ;
+
+
+SELECT YEAR(o.order_date) AS year, p.product_name, SUM((od.price_each * od.quantity_ordered) - (p.buy_price * od.quantity_ordered)) AS total_profit 
+FROM orderdetails AS od, products AS p, orders AS o 
+WHERE od.product_id = p.id AND od.order_id = o.id AND o.order_date IS NOT NULL 
+GROUP BY year, p.product_name 
+ORDER BY year DESC, total_profit DESC;
+
+--  Lets see if there are any prdocut lines that have never had a product ordered
+select * from products;
+
+select *
+from productlines pl
+where pl.id not in ( select p.productline_id from products p, orderdetails od where p.id = od.product_id );
+
+
+select distinct p.productline_id from products p, orderdetails od where p.id = od.product_id;
+
+INSERT INTO `classic_models`.`productlines`
+(
+`product_line`,
+`description`)
+VALUES
+(
+'Test Line',
+'Just for this class');
+
+
