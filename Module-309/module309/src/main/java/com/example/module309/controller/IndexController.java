9ce50359212f9236987d4ae80143a40e9cf21546
Anjaom <eric@webworkbench.net>
2024-12-11 15:03:17 -0700

    added jstl and can show a list of customers on the page

--- Module-309/module309/src/main/java/com/example/module309/controller/IndexController.java
+++ Module-309/module309/src/main/java/com/example/module309/controller/IndexController.java
@@ -29,6 +29,8 @@ public class IndexController {
             System.out.println(c.toString());
         }
 
+        response.addObject("names", firstNames);
+
         return response;
     }
 
--- Module-309/module309/src/main/java/com/example/module309/database/dao/CustomerDAO.java
+++ Module-309/module309/src/main/java/com/example/module309/database/dao/CustomerDAO.java
@@ -5,9 +5,11 @@ import com.example.module309.database.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CustomerDAO extends JpaRepository<Customer, Long> {
 
     @Query("select c from Customer c where c.contactFirstname = :firstName")
