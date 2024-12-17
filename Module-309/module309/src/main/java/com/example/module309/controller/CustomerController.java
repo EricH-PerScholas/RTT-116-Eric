9bcf627f0814b386e219536c301cc2238c11df5e
Anjaom <eric@webworkbench.net>
2024-12-17 09:34:54 -0700

    added logging to project

--- Module-309/module309/src/main/java/com/example/module309/controller/CustomerController.java
+++ Module-309/module309/src/main/java/com/example/module309/controller/CustomerController.java
@@ -5,7 +5,11 @@ import com.example.module309.database.dao.EmployeeDAO;
 import com.example.module309.database.entity.Customer;
 import com.example.module309.database.entity.Employee;
 import com.example.module309.form.CreateCustomerFormBean;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.tags.shaded.org.apache.xpath.operations.Mod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,8 +18,27 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
+
+// serverity of the error message increases going down
+// as developers 90% of the time we are going to use DEBUG
+// TRACE   -- this is very low level and not often used by us .. more for creators of libraries
+// DEBUG   -- this is what we use most of the time when we wnat to print out stuff that helps us build
+// INFO    -- this is for information that is important like the messages that spring prints when it starts up
+// WARN    -- this is a potential problem or something of note but it is not an error
+// ERROR   -- this is for errors like making an api call that failed OR if an exception is thrown
+
+
+// slf4j is not an implementation of logging it is a specification
+// most if not all other logging libraries have come to use this specification for their implementation
+// log4j was the most commonly used logging library for a long time and you will probably encounter it
+//@Slf4j  <-  this is from lombok and all it does is line 40
 @Controller
 public class CustomerController {
+
+    // this is the old style logging before lombok and there is a very good chance you will see this in code somewhere
+    // lombok is preferred
+    private static final Logger LOG = LoggerFactory.getLogger(CustomerController.class);
+
 
     @Autowired
     private CustomerDAO customerDao;
@@ -48,6 +71,11 @@ public class CustomerController {
         // this just shows us the create page for the first time when the user goes to the page
         ModelAndView response = new ModelAndView();
 
+        LOG.debug("DEBUG LEVEL");
+        LOG.info("INFO LEVEL");
+        LOG.warn("WARNING LEVEL");
+        LOG.error("ERROR LEVEL");
+
         response.setViewName("customer/create");
 
         return response;
@@ -60,7 +88,9 @@ public class CustomerController {
 
         response.setViewName("customer/create");
 
-        System.out.println(form);
+        LOG.debug(form.toString());
+
+        System.out.println(form);  //forbidden
 
         Customer customer = new Customer();
         customer.setCustomerName(form.getCompanyName());
