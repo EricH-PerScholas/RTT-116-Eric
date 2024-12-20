90df22fa20fe152e35f401823257fc1ee255e545
Anjaom <eric@webworkbench.net>
2024-12-20 13:55:19 -0700

    added custom validations for email unique and current password

--- Module-309/module309/src/main/java/com/example/module309/form/SignupFormBean.java
+++ Module-309/module309/src/main/java/com/example/module309/form/SignupFormBean.java
@@ -1,13 +1,17 @@
 package com.example.module309.form;
 
+import com.example.module309.validation.EmailUnique;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+// this is optional to figure out if you want to try
+//@TwoFieldsAreEqual(fieldOneName = "confirmPassword", fieldTwoName = "password", message = "Password and Conform Password must be the same.")
 public class SignupFormBean {
 
+    @EmailUnique(message = "Username already exists.")
     @NotEmpty(message = "Username is required")
     private String username;
 
